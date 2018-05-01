fb713d5c5d24a2e7edb71e6e3fd43bc677844bd3
William Powell <william.powell92@gmail.com>
2018-05-01 16:41:43 +0100

    Added attack method to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,15 @@
 class Player
+  STARTING_HEALTH = 50
+  DEFAULT_DAMAGE = 10
 
-  attr_reader :name
-  
+  attr_reader :name, :health
+
   def initialize(name)
     @name = name
+    @health = STARTING_HEALTH
+  end
+
+  def attack
+    @health -= DEFAULT_DAMAGE
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,25 @@ require 'player'
 
 describe Player do
   subject {Player.new("John")}
-  it 'has a name' do
-    expect(subject.name).to eq "John"
+  let(:starting_health) { described_class::STARTING_HEALTH }
+  let(:default_damage) { described_class::DEFAULT_DAMAGE }
+
+  describe '#name' do
+    it 'has a name' do
+      expect(subject.name).to eq "John"
+    end
+  end
+
+  describe '#health' do
+    it 'defaults with 50 health' do
+      expect(subject.health).to eq starting_health
+    end
+  end
+
+  describe '#attack' do
+    it 'reduces health by 10' do
+      subject.attack
+      expect(subject.health).to eq starting_health - default_damage
+    end
   end
 end

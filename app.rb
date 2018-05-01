000b50a17eddabe60e955fa90ef25782a30bc4dd
John Newman <newmanj903@gmail.com>
2018-05-01 14:44:26 +0100

    tests for will vs john

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @name1 = params[:name1]
+    @name2 = params[:name2]
+    erb '<%= "#{@name1} vs #{@name2}" %>' 
   end
 
   run! if app_file == $0
--- spec/features/home_page_says_spec.rb
+++ spec/features/home_page_says_spec.rb
@@ -2,8 +2,10 @@ feature "home page" do
   before do
     visit('/')
   end
-  scenario " outputs 'Testing infrastructure working!'" do
-    expect(page).to have_content('Testing infrastructure working!')
-
+  scenario "expects page to post the names given in the form to the" do
+    fill_in('name1', with: 'John')
+    fill_in('name2', with: 'Will')
+    click_button('Sumbit Players')
+    expect(page).to have_content('John vs Will')
   end
 end
